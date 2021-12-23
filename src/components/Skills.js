9a7fa9a1b2dc9eb992d227b9fb181a7c5b8d663c
Josh Reed <ree17017@byui.edu>
2021-12-23 08:33:49 -0700

    Add all Skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,12 +2,10 @@ import React from "react";
 import useLocalStorage from "react-localstorage-hook";
 import { modifier } from "../tools/modifier";
 import { SkillsEnum } from "./enums";
-import styled from "styled-components";
 
 export default function Skills(props) {
   const [str] = useLocalStorage("str");
   const [dex] = useLocalStorage("dex");
-  const [con] = useLocalStorage("con");
   const [int] = useLocalStorage("int");
   const [wis] = useLocalStorage("wis");
   const [cha] = useLocalStorage("cha");
@@ -43,63 +41,89 @@ export default function Skills(props) {
     <div>
       <h4>Strength</h4>
       <ul>
-        <li>
-          {SkillsEnum.str.athletics}
-          <input
-            type="checkbox"
-            name={SkillsEnum.str.athletics}
-            onChange={(event) => handleOnChange(event)}
-            checked={isChecked(SkillsEnum.str.athletics)}
-          />
-          {skillTotal(SkillsEnum.str.athletics, str)}
-        </li>
+        {Object.keys(SkillsEnum.str).map((skill) => {
+          console.log(skill);
+          return (
+            <Skill
+              name={SkillsEnum.str[skill]}
+              handleOnChange={handleOnChange}
+              isChecked={isChecked(skill)}
+              stat={skillTotal(skill, str)}
+            />
+          );
+        })}
       </ul>
       <h4>Dexterity</h4>
       <ul>
-        <li>
-          {SkillsEnum.dex.acrobatics}
-          <input
-            type="checkbox"
-            name={SkillsEnum.dex.acrobatics}
-            onChange={(event) => handleOnChange(event)}
-            checked={isChecked(SkillsEnum.dex.acrobatics)}
-          />
-          {skillTotal(SkillsEnum.dex.acrobatics, dex)}
-        </li>
-        <li>
-          {SkillsEnum.dex.slight_of_hand}
-          <input
-            type="checkbox"
-            name={SkillsEnum.dex.slight_of_hand}
-            onChange={(event) => handleOnChange(event)}
-            checked={isChecked(SkillsEnum.dex.slight_of_hand)}
-          />{" "}
-          {skillTotal(SkillsEnum.dex.slight_of_hand, dex)}
-        </li>
-        <li>
-          {SkillsEnum.dex.stealth}
-          <input
-            type="checkbox"
-            name={SkillsEnum.dex.stealth}
-            onChange={(event) => handleOnChange(event)}
-            checked={isChecked(SkillsEnum.dex.stealth)}
-          />
-          {skillTotal(SkillsEnum.dex.stealth, dex)}
-        </li>
+        {Object.keys(SkillsEnum.dex).map((skill) => {
+          console.log(skill);
+          return (
+            <Skill
+              name={SkillsEnum.dex[skill]}
+              handleOnChange={handleOnChange}
+              isChecked={isChecked(skill)}
+              stat={skillTotal(skill, dex)}
+            />
+          );
+        })}
       </ul>
       <h4>Intelligence</h4>
       <ul>
-        <li>
-          {SkillsEnum.int.arcana}
-          <input
-            type="checkbox"
-            name={SkillsEnum.int.arcana}
-            onChange={(event) => handleOnChange(event)}
-            checked={isChecked(SkillsEnum.int.arcana)}
-          />
-          {skillTotal(SkillsEnum.int.arcana, int)}
-        </li>
+        {Object.keys(SkillsEnum.int).map((skill, index) => {
+          console.log(skill);
+          return (
+            <Skill
+              name={SkillsEnum.int[skill]}
+              handleOnChange={handleOnChange}
+              isChecked={isChecked(skill)}
+              stat={skillTotal(skill, int)}
+            />
+          );
+        })}
+      </ul>
+      <h4>Wisdom</h4>
+      <ul>
+        {Object.keys(SkillsEnum.wis).map((skill) => {
+          console.log(skill);
+          return (
+            <Skill
+              name={SkillsEnum.wis[skill]}
+              handleOnChange={handleOnChange}
+              isChecked={isChecked(skill)}
+              stat={skillTotal(skill, wis)}
+            />
+          );
+        })}
+      </ul>
+      <h4>Charisma</h4>
+      <ul>
+        {Object.keys(SkillsEnum.cha).map((skill) => {
+          console.log(skill);
+          return (
+            <Skill
+              name={SkillsEnum.cha[skill]}
+              handleOnChange={handleOnChange}
+              isChecked={isChecked(skill)}
+              stat={skillTotal(skill, cha)}
+            />
+          );
+        })}
       </ul>
     </div>
+  );
+}
+
+function Skill({ name, handleOnChange, isChecked, stat }) {
+  return (
+    <li>
+      <input
+        type="checkbox"
+        name={name.toLowerCase()}
+        onChange={(event) => handleOnChange(event)}
+        checked={isChecked}
+      />
+      <span>{name}</span>
+      <span>{stat}</span>
+    </li>
   );
 }
