ff9acd2691f23929002e5d512946f9a059c52211
Josh Reed <ree17017@byui.edu>
2021-12-24 02:55:27 -0700

    Add Character and Player info fields

--- src/components/CharacterInfo.js
+++ src/components/CharacterInfo.js
@@ -2,76 +2,80 @@ import React, { useEffect } from "react";
 import useLocalStorage from "react-localstorage-hook";
 
 export default function CharacterInfo(props) {
-  const [characterInfo, setCharacterInfo] = useLocalStorage("characterInfo");
-
-  const handleCharacterInfo = (event) => {
-    setCharacterInfo(...characterInfo, event.target.value);
-  };
+  const [characterName, setCharacterName] = useLocalStorage("characterInfo");
+  const [characterclass, setCharacterClass] = useLocalStorage("class");
+  const [level, setLevel] = useLocalStorage("level");
+  const [background, setBackground] = useLocalStorage("background");
+  const [playerName, setPlayerName] = useLocalStorage("playername");
+  const [race, setRace] = useLocalStorage("race");
+  const [speed, setSpeed] = useLocalStorage("speed");
+  const [alignment, setAlignment] = useLocalStorage("alignment");
+  const [experience, setExperience] = useLocalStorage("experience");
 
   return (
     <div>
       Character Name:
       <input
         type="text"
-        value={characterInfo.name}
-        onChange={(event) => handleCharacterInfo(event)}
+        value={characterName}
+        onChange={(event) => setCharacterName(event.target.value)}
         name="characterName"
       />
       Class:
       <input
         type="text"
         name="class"
-        value={characterInfo.class}
-        onChange={(event) => handleCharacterInfo(event)}
+        value={characterclass}
+        onChange={(event) => setCharacterClass(event.target.value)}
       />
       Level:
       <input
         type="number"
         name="level"
-        value={characterInfo.level}
-        onChange={(event) => handleCharacterInfo(event)}
+        value={level}
+        onChange={(event) => setLevel(event.target.value)}
       />
       Background:
       <input
         type="text"
         name="Background"
-        value={characterInfo.background}
-        onChange={(event) => handleCharacterInfo(event)}
+        value={background}
+        onChange={(event) => setBackground(event.target.value)}
       />
       Player Name:
       <input
         type="text"
         name="playerName"
-        value={characterInfo.playerName}
-        onChange={(event) => handleCharacterInfo(event)}
+        value={playerName}
+        onChange={(event) => setPlayerName(event.target.value)}
       />
       Race:
       <input
         type="text"
         name="race"
-        value={characterInfo.race}
-        onChange={(event) => handleCharacterInfo(event)}
+        value={race}
+        onChange={(event) => setRace(event.target.value)}
       />
       Speed:
       <input
         type="text"
         name="speed"
-        value={characterInfo.speed}
-        onChange={(event) => handleCharacterInfo(event)}
+        value={speed}
+        onChange={(event) => setSpeed(event.target.value)}
       />
       Alignment:
       <input
         type="text"
         name="alignment"
-        value={characterInfo.alignment}
-        onChange={(event) => handleCharacterInfo(event)}
+        value={alignment}
+        onChange={(event) => setAlignment(event.target.value)}
       />
       Experience Points:
       <input
         type="text"
         name="experience"
-        value={characterInfo.experience}
-        onChange={(event) => handleCharacterInfo(event)}
+        value={experience}
+        onChang={(event) => setExperience(event.target.value)}
       />
     </div>
   );
